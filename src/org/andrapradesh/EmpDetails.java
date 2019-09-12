5f823d8b970bb6a51c803e9dd895988c7c07b914
vpriya2209 <priyavishnu621@gmail.com>
2019-09-12 15:25:24 +0530

    new program

---
+++ src/org/andrapradesh/EmpDetails.java
@@ -0,0 +1,13 @@
+package org.andrapradesh;
+
+public class EmpDetails {
+	private void empID() {
+		System.out.println("hello");
+
+	}
+	
+	public static void main(String[] args) {
+		EmpDetails a = new EmpDetails ();
+		a.empID();
+	}
+}
